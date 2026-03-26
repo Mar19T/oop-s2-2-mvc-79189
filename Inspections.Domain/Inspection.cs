a2766eda6e1a8b22fc1153d80b01c4fb80f1a68a
Mario Tejeda <marioandredub@gmail.com>
2026-03-26 09:29:49 +0000

    Checking workflow functionality

--- Inspections.Domain/Inspection.cs
+++ Inspections.Domain/Inspection.cs
@@ -13,7 +13,7 @@ namespace Inspections.Domain
             [Required]
             public int PremisesId { get; set; }
             public Premises? Premises { get; set; }
-            public DateTime InspectionDate { get; set; }
+            public DateTime InspectionDate { get; set; }  
             public int Score { get; set; }
             public string Outcome { get; set; }
             public string Notes { get; set; }
